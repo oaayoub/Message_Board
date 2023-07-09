b4579ccf6338f475b441de9c2bb6906293ed5075
oaayoub <oaayoub0101@gmail.com>
2023-07-09 19:29:23 +0300

    Added some stuff

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,17 @@
 class MessagesController < ApplicationController
+    def index
+
+    end
+    def new
+        @message = Message.new #Message is the model class (db)
+    end
+    def create
+        @message = Message.new(message_params)
+    end
+
+    private
+    def message_params
+        params.require(:message).permit(:title,:description) #Allow only title and description params and clip the others
+
+    end
 end
