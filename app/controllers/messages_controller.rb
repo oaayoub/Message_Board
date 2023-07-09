6636b4ff9fd562038034606baf5550e6f88d532c
oaayoub <oaayoub0101@gmail.com>
2023-07-09 23:07:32 +0300

    Added show create index routes and pages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,21 @@
 class MessagesController < ApplicationController
     def index
-
+        @messages = Message.all.order("created_at DESC")
+    end
+    def show
+        @message = Message.find(params[:id])
     end
     def new
         @message = Message.new #Message is the model class (db)
+
     end
     def create
         @message = Message.new(message_params)
+        if @message.save
+            redirect_to root_path
+        else
+            render 'new'
+        end
     end
 
     private
